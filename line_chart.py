2b1c6253b56c25769efc29047ff061ffafed932b
Kayci Messerly <kim0009@auburn.edu>
2023-04-16 10:10:27 -0500

    Output file name update.

--- line_chart.py
+++ line_chart.py
@@ -18,7 +18,7 @@ sns.set_palette(sns.color_palette(colors))
 p = sns.relplot(data=population_long, x="year", y="change", hue="species", kind="line", height=5, aspect=2)
 p.set(xlabel="Year", ylabel="Relative Change in European Bird Population", title="Relative Population Change by European Bird Species")
 p.tight_layout()
-p.savefig("line_graph.png", dpi=300)
+p.savefig("line_chart.png", dpi=300)
 
 # Move the legend to the lower left of the plot
 p._legend.set_bbox_to_anchor((0, 0.5))
